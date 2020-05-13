376cea253668518c82506bea602b9bca6c298a47
Jon Whithouse <flatironschool@foxlawn.plus.com>
2020-05-13 10:21:44 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,7 +6,7 @@ function updatePlaylist(playlist,artist,song){
   return playlist
   }
 
-function removeFromPlayList(playlist,artist){
+function removeFromPlaylist(playlist,artist){
 delete playlist.artist;
   return playlist
 }
